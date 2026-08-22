2bfe50dd497e91a6fbd720069fd90200bbcf0383
rafaacss <rafa.acss@gmail.com>
2026-08-22 20:32:29 -0300

    feat: aplica Bootstrap e traduz interface para português

--- src/Form/LivroType.php
+++ src/Form/LivroType.php
@@ -7,6 +7,9 @@ use App\Entity\Autor;
 use App\Entity\Livro;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,24 +18,71 @@ class LivroType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Titulo')
-            ->add('Editora')
-            ->add('Edicao')
-            ->add('AnoPublicacao')
-            ->add('Valor')
+            ->add('Titulo', TextType::class, [
+                'label' => 'Título',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Digite o título do livro',
+                ],
+            ])
+
+            ->add('Editora', TextType::class, [
+                'label' => 'Editora',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Digite a editora',
+                ],
+            ])
+
+            ->add('Edicao', IntegerType::class, [
+                'label' => 'Edição',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ex.: 18',
+                ],
+            ])
+
+            ->add('AnoPublicacao', TextType::class, [
+                'label' => 'Ano de publicação',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ex.: 2024',
+                    'maxlength' => 4,
+                ],
+            ])
+
+            ->add('Valor', NumberType::class, [
+                'label' => 'Valor',
+                'scale' => 2,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ex.: 133.90',
+                ],
+            ])
+
             ->add('assuntos', EntityType::class, [
                 'class' => Assunto::class,
                 'choice_label' => 'Descricao',
                 'choice_value' => 'codAs',
                 'multiple' => true,
+                'label' => 'Assuntos',
+                'attr' => [
+                    'class' => 'form-select',
+                    'size' => 5
+                ],
             ])
+
             ->add('autores', EntityType::class, [
                 'class' => Autor::class,
                 'choice_label' => 'Nome',
                 'choice_value' => 'CodAu',
                 'multiple' => true,
-            ])
-        ;
+                'label' => 'Autores',
+                'attr' => [
+                    'class' => 'form-select',
+                    'size' => 5
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
