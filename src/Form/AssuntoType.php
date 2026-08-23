94aeef04eb6c258efb57715b208274035063a390
rafaacss <rafa.acss@gmail.com>
2026-08-23 15:06:30 -0300

    feat: Adicinando menu lateral e mais alterações de layout das telas

--- src/Form/AssuntoType.php
+++ src/Form/AssuntoType.php
@@ -6,6 +6,7 @@ use App\Entity\Assunto;
 use App\Entity\Livro;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,13 @@ class AssuntoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Descricao')
+            ->add('Descricao', TextType::class, [
+                'label' => 'Descrição',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Digite uma descrição para o assuntoo do livro',
+                ],
+            ])
         ;
     }
 
--- src/Form/AutorType.php
+++ src/Form/AutorType.php
@@ -3,9 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Autor;
-use App\Entity\Livro;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +13,13 @@ class AutorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nome')
+            ->add('Nome', TextType::class, [
+                'label' => 'Nome',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Digite o nome do autor',
+                ],
+            ])
         ;
     }
 
