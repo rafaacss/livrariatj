46ce95f2b0ddf24d0cc566c35eea69ea1afabe44
rafaacss <rafa.acss@gmail.com>
2026-08-23 16:45:26 -0300

    feat: Adicionando gerador de PDf para os relatórios

--- src/Controller/RelatorioController.php
+++ src/Controller/RelatorioController.php
@@ -8,6 +8,8 @@ use Doctrine\DBAL\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 #[Route('/relatorio')]
 final class RelatorioController extends AbstractController
@@ -29,5 +31,53 @@ final class RelatorioController extends AbstractController
         return $this->render('relatorio/livros.html.twig', [
             'livros' => $relatorioLivroService->buscarRelatorio(),
         ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('/livros/pdf',name: 'app_relatorio_pdf_livros', methods: ['GET'])]
+    public function livroPdf(RelatorioLivroService $relatorioLivroService): Response
+    {
+        $html = $this->renderView('relatorio/livros.pdf.html.twig', [
+            'livros' => $relatorioLivroService->buscarRelatorio(),
+        ]);
+
+        return new Response($this->gerarPdf($html)->output(), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="relatorio.pdf"',
+        ]);
+    }
+
+    #[Route('/autor/pdf',name: 'app_relatorio_pdf_autores', methods: ['GET'])]
+    public function autorPdf(RelatorioAutorService $relatorioAutorService): Response
+    {
+        $html = $this->renderView('relatorio/autores.pdf.html.twig', [
+            'autores' => $relatorioAutorService->buscarRelatorio(),
+        ]);
+
+        return new Response($this->gerarPdf($html)->output(), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="relatorio.pdf"',
+        ]);
+    }
+
+    private function gerarPdf ($html): Dompdf
+    {
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'DejaVu Sans');
+
+        $dompdf = new Dompdf($pdfOptions);
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream('relatorio.pdf');
+
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+
+        return $dompdf;
+
     }
 }
